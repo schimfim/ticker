96d2f5fe8d64c3ec2d0f075a749a3a314b832f00
uncleWalter <schimfim@mac.com>
2013-08-03 05:29:41 -0700

    Auto commit by giteasy

---
+++ store.py
@@ -0,0 +1,30 @@
+# store.py
+
+import shelve
+
+# Interface and shelve implementation
+class Store(object):
+	def __init__(self):
+		self.db = {}
+		
+	def open(self, name):
+		self.db = shelve.open(name)
+		# return self.db
+		
+	def read(self, id):
+		d = {}
+		if(id in self.db):
+			d = self.db[id]
+		return d
+		
+	def write(self, id, d):
+		self.db[id] = d
+		
+	def close(self):
+		self.db.close()
+		
+	def list(self):
+		return self.db.keys()
+	
+	def delete(self, id):
+		del self.db[id]
