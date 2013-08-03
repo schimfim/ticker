18b04e2c21b2ac88fbde2e0a94bd0db8ef4f4473
uncleWalter <schimfim@mac.com>
2013-08-03 05:29:49 -0700

    Auto commit by giteasy

---
+++ sync.py
@@ -0,0 +1,22 @@
+from giteasy import RepoCLI
+
+files = [
+	'FussiTicker.py',
+	'FussiTicker_test.py',
+	'FussiWeb.py',
+	'config.py',
+	'init_tickerdb.py',
+	'store.py',
+	'store_sqlite.py',
+	'store_test.py',
+	'sync.py',
+	'ticker_tpl.py'
+	]
+
+'''
+files = [
+	'FussiTicker.py'
+	]
+'''
+
+RepoCLI('ticker', 'schimfim', 'Ninz2009', files)
