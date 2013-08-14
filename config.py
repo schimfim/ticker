8363fd287e12f446045358c2309d36f845915bdd
uncleWalter <schimfim@mac.com>
2013-08-14 09:44:02 -0700

    Auto commit by giteasy

--- config.py
+++ config.py
@@ -4,23 +4,26 @@
 mode = ['iphone', 'mac', 'appengine'][0]
 
 if mode == 'iphone':
-	# Storage
 	import store_sqlite as store
 	ahost = 'localhost'
 	aport = 8080
+	run_opts = {'host':ahost, 'port':aport}
 elif mode == 'mac':
-	# Storage
 	import store_sqlite as store
-	ahost = 'localhost'
-	aport = 8080
+	ahost = '192.168.1.99'
+	aport = 80
+	run_opts = {'host':ahost, 'port':aport}
 elif mode == 'appengine':
-	# Storage
 	import store_sqlite as store
 	ahost = 'localhost'
 	aport = 8080
+	run_opts = {'server':'gae'}
 else: 
 	raise ValueError('Unknown mode: {}'.format(mode))
 
+web_opts = {'stop_when_done' : True}
+
+# 
 ticks = store.Store()
 base_url = 'http://' + ahost + ':' + str(aport)
 
