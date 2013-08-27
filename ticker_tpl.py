eda167f4a65d5fb9a41d1ba2a55ed3fe8d0bb895
uncleWalter <schimfim@mac.com>
2013-08-27 09:30:28 -0700

    Auto commit by giteasy

--- ticker_tpl.py
+++ ticker_tpl.py
@@ -1,39 +1,42 @@
 tpl = '''
 <html><head>
+	<meta http-equiv="expires" content="10"/>
+	<meta http-equiv="refresh" content="10; URL=/{{mid}}" /> 
 			<style type="text/css">
-			h1 { color:blue; font-size:64px; }
-			input[type=submit] {font-size:48px;}
-			input[type=text] {font-size:48px;}
-			p {font-size:48px;}
-			table { width:100%; }
-			td { border:1px solid #000; vertical-align:top; overflow:hidden; font-size:13px; font-family:Verdana,sans-serif; font-weight:bold;}
+			body {background-color:green; font-family:sans-serif}
+			h1 { font-size:3cm; text-align:center; background-color:white;}
+			input[type=submit] {background-color:transparent; width:100%; color:white; font-size:2cm; border-style:none}
+			input[type=text] {font-size:2cm; width:100%; background-color:transparent; color:white}
+			p {font-size:48px; align:center;}
+			table { width:100%; border-spacing:0px; border-collapse:collapse; table-layout:fixed}
+			td { border:10px solid white; vertical-align:center; align:center; overflow:hidden; font-size:3cm; font-weight:bold; background-color:green;}
+			input[type=submit].score {color:white; font-size:5cm;}
 			</style>
 		</head>
 <body>
-	<h1>Ticker {{mid}}</h1>
-		<p><a href="{{mail}}">Mail</a></p>
-		<p>
-		%for si in mids:
-			<a href="/{{si}}">{{si}}</a>
-			<a href="/del/{{si}}"> [del]</a>
-		%end
-		</p>
-		
+	<h1>Fussi Ticker</h1>
 		<form action="/{{mid}}" method="POST">
 			<table>
 			<tr>
-			<td colspan=3><input type="text" name="hteam" value="{{hteam}}"></td>
+			<td colspan=2 align="center">
+			%if running:
+				<input type="text" name="hteam" style="background-color:red"; value="{{hteam}}">
+			%else:
+				<input type="text" name="hteam" value="{{hteam}}">
+			%end
+			</td>
 			</tr>
-			<tr>
-			<td><input type="submit" name="home_u" value="{{home}}"></td>
-			<td>:</td>
-			<td><input type="submit" name="home_d" value="{{guest}}"></td>
+			<tr style="height:6cm;">
+			<td><input class="score" type="submit" name="home_u" value="{{home}}"></td>
+			<td><input class="score" type="submit" name="guest_u" value="{{guest}}"></td>
 			</tr>
-			<tr>
-			<td><input type="submit" name="guest_u" value="Guest+"></td>
-			<td></td>
-			<td><input type="submit" name="guest_d" value="Guest-"></td>
+			<tr style="height:2cm;">
+			<td><input type="submit" name="home_d" value="-"></td>
+			<td><input type="submit" name="guest_d" value="-"></td>
 			</tr>
+		</table>
+		<br>
+		<table>
 			<tr>
 			<td><input type="submit" name="new" value="New"></td>
 			<td><input type="submit" name="start" value="Start"></td>
@@ -42,13 +45,7 @@ tpl = '''
 			
 			</table>
 		</form>
-	<h1>Output</h1>
-	%if running:
-		<p>Running</p>
-	%else:
-		<p>Game over</p>
-	%end
-		<p>{{home}} : {{guest}}</p>
+		<p><a href="{{mail}}">Share this Ticker</a></p>
 </body></html>
 '''
 
