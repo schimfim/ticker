4741e03447ef2ff27f19686d0f299544fadb997a
uncleWalter <schimfim@mac.com>
2013-09-06 12:38:23 -0700

    Auto commit by giteasy

--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 # Imports
 from bottle import ConfigDict
 import re
+import logging
+logging.basicConfig(level=logging.INFO)
  
 #Config
 #config = ConfigDict()
@@ -14,26 +16,35 @@ with open('ticker.cfg') as f:
 	s = f.readline()
 r = re.match('.*=([a-z]+)', s)
 mode = r.group(1)
-print 'mode=', mode
+logging.info('mode = ' + mode)
 
 if mode == 'iphone':
 	import store_sqlite as store
 	ahost = 'localhost'
 	aport = 8080
 	run_opts = {'host':ahost, 'port':aport}
-	web_opts = {'stop_when_done' : False , 'modal' : False  }
+	web_opts = {'stop_when_done' : True, 'modal' : False  }
+	refresh = 3000
+	debug = True 
+	catchall=False 
 elif mode == 'mac':
 	import store_sqlite as store
 	ahost = 'localhost'
 	aport = 8088
 	run_opts = {'host':ahost, 'port':aport}
 	web_opts = {}
+	refresh = 3000
+	debug = True
+	catchall=False
 elif mode == 'appengine':
 	import store_ndb as store
 	ahost = 'fussiticker.appspot.com'
 	aport = 80
 	run_opts = {'server':'gae'}
 	web_opts = {}
+	refresh = 10000
+	debug = False
+	catchall=True 
 else: 
 	raise ValueError('Unknown mode: {}'.format(mode))
 
